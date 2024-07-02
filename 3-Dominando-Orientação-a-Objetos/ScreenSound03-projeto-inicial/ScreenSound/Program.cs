0bb9fb8d15528baf9731f88622eada9d1553ee80
Lucas Moretti <Lucas.morettic5@gmail.com>
2024-07-02 13:14:38 -0300

    Add

--- 3-Dominando-Orientação-a-Objetos/ScreenSound03-projeto-inicial/ScreenSound/Program.cs
+++ 3-Dominando-Orientação-a-Objetos/ScreenSound03-projeto-inicial/ScreenSound/Program.cs
@@ -149,35 +149,7 @@ void AvaliarUmaBanda()
         ExibirOpcoesDoMenu();
     }
 
-}
 
-void ExibirDetalhes()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Exibir detalhes da banda");
-    Console.Write("Digite o nome da banda que deseja conhecer melhor: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-        Banda banda = bandasRegistradas[(nomeDaBanda)];
-        Console.WriteLine($"\nA média da banda {nomeDaBanda} é {banda.Media}.");
-        /**
-        * ESPAÇO RESERVADO PARA COMPLETAR A FUNÇÃO
-        */
-        Console.WriteLine("Digite uma tecla para votar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
 
-    }
-    else
-    {
-        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
-}
 
 ExibirOpcoesDoMenu();
